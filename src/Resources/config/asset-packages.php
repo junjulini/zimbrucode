93febe961250bfc90df2bc0991529d78a2e4e5a2
C.R <const.513@gmail.com>
2024-12-03 19:31:36 +0200

    Bump to version 1.3.3

--- src/Resources/config/asset-packages.php
+++ src/Resources/config/asset-packages.php
@@ -40,7 +40,7 @@ return [
                         return "{$this->path}/packages/zimbrucode/jquery.zimbrucode.js";
                     }
                 })(),
-                'version' => '1.3.2',
+                'version' => '1.3.3',
             ],
         ],
     ],
